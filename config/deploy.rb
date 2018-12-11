dc8ac26a000a9625bc7d2786e2cdae9bc2fdbf38
faisal farooq <chaudhary.fsl@gmail.com>
2018-12-10 22:27:56 -0500

    initial commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@
 
 
 set :application, 'aws'
-set :repo_url, 'git@github.com:devdatta/contactbook.git' # Edit this to match your repository
+set :repo_url, 'git@github.com:chaudhary27/aws.git' # Edit this to match your repository
 set :branch, :master
 set :deploy_to, '/home/deploy/contactbook'
 set :pty, true
