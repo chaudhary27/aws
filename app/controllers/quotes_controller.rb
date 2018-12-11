e1db4026d66ee74a00ce5ac2c46656d6591aa973
faisal farooq <chaudhary.fsl@gmail.com>
2018-12-11 17:31:59 -0500

    simple form

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,4 +2,32 @@ class QuotesController < ApplicationController
     def index
     @quote = Quote.order("RANDOM()").first
     end
+    	def create
+		@quote = Quote.create(quote_params)
+
+		if @quote.invalid?
+			flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+		end
+
+		redirect_to root_path
+
+	end
+
+	def about
+		
+	end
+
+	def show
+		@quote = Quote.where(:id => params[:id]).first
+		if @quote.blank?
+			render :text => "You should not be here! Go Back HOME", :status => :not_found
+		end
+	end
+
+	private
+
+	def quote_params
+		params.require(:quote).permit(:saying, :author)
+		
+	end
 end
